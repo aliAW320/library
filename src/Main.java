aa45d1344e4c321f5bd32eb7995e0a3bb01996d7
ali noroozi <alinoroozi13845132@gmail.com>
2024-03-16 01:45:00 +0330

    library add commend complited and tests worked

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,58 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 class Library {
     String id;
     String name ;
     String year;
     String numberOfDesk;
-    String adrres;
+    String address;
 
-    Library(String id,String name,String year,String numberOfDesk,String adrres){
+    Library(String id,String name,String year,String numberOfDesk,String address){
         this.id=id;
         this.name=name;
         this.year=year;
         this.numberOfDesk=numberOfDesk;
-        this.adrres=adrres;
+        this.address=address;
     }
 
     public String getId() {
         return id;
     }
 }
+
 public class Main {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String order="";
         ArrayList<Library> libraries = new ArrayList<>();
-        while (!(order.equals("Finish"))){
+        HashMap <String,Library> nameAndId = new HashMap<>();
+        while (!(order.equals("finish"))){
             order= input.nextLine();
-            findOrder(order,libraries);
+            findOrder(order,libraries,nameAndId);
 
         }
     }
-public static void findOrder(String order, ArrayList<Library> libraries){
-        if (order.contains("Add-library")){
-            addLibrary(order,libraries);
+public static void findOrder(String order, ArrayList<Library> libraries,HashMap <String,Library> nameAndId){
+        if (order.contains("add-library")){
+            addLibrary(order,libraries,nameAndId);
         }
 }
 
-    private static void addLibrary(String order, ArrayList<Library> libraries) {
+    private static void addLibrary(String order, ArrayList<Library> libraries,HashMap <String,Library
+            > nameAndId) {
         String[] info = order.split("\\|");
         String[] id= info[0].split("\\#");
         boolean duplicate = false;
-        for (int i=0;i<libraries.size();i++){
-
+        if(!(nameAndId.containsKey(id[1]))){
+            libraries.add(new Library(id[1],info[1],info[2],info[3],info[4]));
+            nameAndId.put(id[1],libraries.get(libraries.size()-1));
+            System.out.println("success");
         }
-        libraries.add(new Library(id[1],info[1],info[2],info[3],info[4]));
+        else{
+            System.out.println("duplicate-id");
+        }
 
     }
 
